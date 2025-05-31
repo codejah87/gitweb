2932d22ff85ebbaccc10d7c27e7ac68c3089f661
codejah dev <ehikwejahswill11@gmail.com>
2025-05-30 18:30:12 -0700

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Home from './Pages/Home'
 import Navbar from './Component/NavBar'
-import { Route,  Routes } from 'react-router'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Product from './Pages/Product'
 import Contact from './Pages/Contact'
 import Footer from './Component/Footer'
@@ -16,12 +16,16 @@ const App = () => {
      <Navbar/>
      
    <div className='w-full px-5 sm:px-8 xl:px-29'>
+    <Router>  
+
        <Routes>
          <Route path='/' element={<Home/>}/>
          <Route path='/service' element={<Service/>}/>
          <Route path='/product' element={<Product/>}/>
          <Route path='/contact' element={<Contact/>}/>
        </Routes>
+    </Router>
+
    </div>
    <WhatApp/>
    <Footer/>
